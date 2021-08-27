d80e4b5c9c69f3d8d297ae17ec4bc742bf00867c
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2021-08-27 17:03:06 +0200

    working generator and auto generation on compilation

---
+++ Sources/ImagesGen.h
@@ -0,0 +1,21 @@
+/** Generated on 2021-08-27 17:01:18.923031 **/
+
+#pragma once
+
+#include <string>
+
+namespace Images {
+    namespace Consts {
+
+        extern const std::string headlightsHighBeam;
+        extern const std::string headlightsLowBeam;
+        extern const std::string headlightsOff;
+        extern const std::string rainLightOff;
+        extern const std::string rainLightOn;
+        extern const std::string wipersOff;
+        extern const std::string wipersOn1;
+        extern const std::string wipersOn2;
+        extern const std::string wipersOn3;
+
+    } // namespace Consts
+} // namespace Images
